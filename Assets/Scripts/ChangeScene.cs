daa3232111193c4cf1edb7eea8a32db82c4f9a6f
Muhamad Hariyanto <reinswordk@gmail.com>
2021-09-21 21:31:24 +0700

    First Commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void gantiScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/Owl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Owl : Bird
+{
+
+    [SerializeField]
+    public float fieldOfImpact = 0.4f;
+    public float force = 0.6f;
+
+    public LayerMask LayerToHit;
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            Explode();
+            //Destroy(other.gameObject);
+            
+        }
+    }
+
+    void Explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldOfImpact, LayerToHit);
+        foreach(Collider2D obj in objects)
+        {
+            Vector2 direction = obj.transform.position - transform.position;;
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+        }
+    }
+}
